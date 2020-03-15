de64d0a23ef83809db42310c692a9c4c8197f602
CharlesG-Branch <cgilliam@branch.io>
2020-03-15 01:03:18 -0700

    Disable entry when the field is null

--- DiscoLinkTester/src/main/java/io/branch/search/linktest/util/BranchTextEntry.java
+++ DiscoLinkTester/src/main/java/io/branch/search/linktest/util/BranchTextEntry.java
@@ -6,44 +6,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import io.branch.search.linktest.R;
 
-public class BranchTextEntry extends Fragment {
-    View mView;
+public class BranchTextEntry extends Fragment implements CompoundButton.OnCheckedChangeListener {
+    private TextView header;
+    private EditText entryForm;
+    private CheckBox nullCheckbox;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.branch_text_entry, container, false);
+        View mView = inflater.inflate(R.layout.branch_text_entry, container, false);
+
+        header = mView.findViewById(R.id.branchTextEntryHeader);
+        entryForm = mView.findViewById(R.id.branchTextEntryBox);
+        nullCheckbox = mView.findViewById(R.id.branchTextEntryIsNull);
+
+        nullCheckbox.setOnCheckedChangeListener(this);
+
         return mView;
     }
 
     public void setHeader(String header) {
-        TextView t = mView.findViewById(R.id.branchTextEntryHeader);
-        t.setText(header);
+        this.header.setText(header);
     }
 
     public void setValues(String content) {
         Boolean isNull = content == null;
 
-        ((CheckBox) mView.findViewById(R.id.branchTextEntryIsNull)).setChecked(isNull);
-        ((EditText) mView.findViewById(R.id.branchTextEntryBox)).getText().clear();
+        nullCheckbox.setChecked(isNull);
+        entryForm.getText().clear();
         if (!isNull) {
-            ((EditText) mView.findViewById(R.id.branchTextEntryBox)).getText().append(content);
+            entryForm.getText().append(content);
         }
     }
 
     public String getEnteredValue() {
-        String content = ((EditText) mView.findViewById(R.id.branchTextEntryBox)).getText().toString();
-        Boolean isNull = ((CheckBox) mView.findViewById(R.id.branchTextEntryIsNull)).isChecked();
+        String content = entryForm.getText().toString();
+        Boolean isNull = nullCheckbox.isChecked();
 
         if (isNull) {
             return null;
         } else {
             return content;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            entryForm.setEnabled(false);
+        } else {
+            entryForm.setEnabled(true);
         }
     }
 }
