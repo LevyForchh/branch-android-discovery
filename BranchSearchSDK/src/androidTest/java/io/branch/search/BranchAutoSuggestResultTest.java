ddf96006f34a7e2111817f84ccdf0a67e5003488
Mattia Iavarone <mat.iavarone@gmail.com>
2020-04-06 17:30:46 -0300

    Add tests

--- BranchSearchSDK/src/androidTest/java/io/branch/search/BranchAutoSuggestResultTest.java
+++ BranchSearchSDK/src/androidTest/java/io/branch/search/BranchAutoSuggestResultTest.java
@@ -10,40 +10,40 @@ import org.junit.runner.RunWith;
 import io.branch.search.util.AssetUtils;
 
 /**
- * {@link BranchQueryHintResult} Test.
+ * {@link BranchAutoSuggestResult} Test.
  */
 @RunWith(AndroidJUnit4.class)
-public class BranchQueryHintResultTest extends BranchTest {
+public class BranchAutoSuggestResultTest extends BranchTest {
 
     @Test
     public void testResultSuccess() throws Throwable {
-        String response = AssetUtils.readJsonFile(getTestContext(), "success_queryhint.json");
+        String response = AssetUtils.readJsonFile(getTestContext(), "success_autosuggest.json");
         Assert.assertTrue(response.length() > 0);
 
         JSONObject jsonResponse = new JSONObject(response);
 
-        BranchQueryHintResult result = BranchQueryHintResult.createFromJson(jsonResponse);
+        BranchAutoSuggestResult result = BranchAutoSuggestResult.createFromJson(jsonResponse);
         Assert.assertNotNull(result);
 
-        Assert.assertEquals(3, result.getHints().size());
+        Assert.assertEquals(3, result.getSuggestions().size());
     }
 
     @Test
     public void testResultSuccess_empty() throws Throwable {
-        String response = AssetUtils.readJsonFile(getTestContext(), "success_queryhint_empty.json");
+        String response = AssetUtils.readJsonFile(getTestContext(), "success_autosuggest_empty.json");
         Assert.assertTrue(response.length() > 0);
 
         JSONObject jsonResponse = new JSONObject(response);
 
-        BranchQueryHintResult result = BranchQueryHintResult.createFromJson(jsonResponse);
+        BranchAutoSuggestResult result = BranchAutoSuggestResult.createFromJson(jsonResponse);
         Assert.assertNotNull(result);
 
-        Assert.assertEquals(0, result.getHints().size());
+        Assert.assertEquals(0, result.getSuggestions().size());
     }
 
     @Test
     public void testResultError() throws Throwable {
-        String response = AssetUtils.readJsonFile(getTestContext(), "err_queryhint.json");
+        String response = AssetUtils.readJsonFile(getTestContext(), "err_autosuggest.json");
         Assert.assertTrue(response.length() > 0);
 
         JSONObject jsonObject = new JSONObject(response);
