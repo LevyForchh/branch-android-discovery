e039acaa61245bf0da2cd6375888f3d5d308c37f
bklastaitis-branch <benas.klastaitis@branch.io>
2020-04-06 18:31:40 -0700

    add back in IBranchInitEvents, mark as deprecated

---
+++ BranchSearchSDK/src/main/java/io/branch/search/IBranchInitEvents.java
@@ -0,0 +1,11 @@
+package io.branch.search;
+
+/**
+ * @deprecated Events called  when Branch Search SDK is initialized.
+ */
+public interface IBranchInitEvents {
+    /**
+     * @deprecated not used anymore/no-op
+     */
+    void onOptionalLocationPermissionMissing();
+}
