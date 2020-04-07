3c0c31e6e0f39548607cdbc96ae7399ae8900c60
Mattia Iavarone <mat.iavarone@gmail.com>
2020-04-07 12:03:54 -0300

    Make BranchAutoSuggestion and BranchQueryHint parcelable

--- BranchSearchSDK/src/main/java/io/branch/search/BranchAutoSuggestion.java
+++ BranchSearchSDK/src/main/java/io/branch/search/BranchAutoSuggestion.java
@@ -1,11 +1,13 @@
 package io.branch.search;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 /**
  * Represents a single auto suggest result.
  */
-public class BranchAutoSuggestion {
+public class BranchAutoSuggestion implements Parcelable {
     private final String query;
 
     BranchAutoSuggestion(@NonNull String query) {
@@ -22,4 +24,27 @@ public class BranchAutoSuggestion {
     public String toString() {
         return query;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(query);
+    }
+
+    public final static Creator<BranchAutoSuggestion> CREATOR = new Creator<BranchAutoSuggestion>() {
+        @Override
+        public BranchAutoSuggestion createFromParcel(Parcel source) {
+            //noinspection ConstantConditions
+            return new BranchAutoSuggestion(source.readString());
+        }
+
+        @Override
+        public BranchAutoSuggestion[] newArray(int size) {
+            return new BranchAutoSuggestion[size];
+        }
+    };
 }
--- BranchSearchSDK/src/main/java/io/branch/search/BranchQueryHint.java
+++ BranchSearchSDK/src/main/java/io/branch/search/BranchQueryHint.java
@@ -1,11 +1,13 @@
 package io.branch.search;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 /**
  * Represents a single query hint result.
  */
-public class BranchQueryHint {
+public class BranchQueryHint implements Parcelable {
     private final String query;
 
     BranchQueryHint(@NonNull String query) {
@@ -22,4 +24,27 @@ public class BranchQueryHint {
     public String toString() {
         return query;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(query);
+    }
+
+    public final static Creator<BranchQueryHint> CREATOR = new Creator<BranchQueryHint>() {
+        @Override
+        public BranchQueryHint createFromParcel(Parcel source) {
+            //noinspection ConstantConditions
+            return new BranchQueryHint(source.readString());
+        }
+
+        @Override
+        public BranchQueryHint[] newArray(int size) {
+            return new BranchQueryHint[size];
+        }
+    };
 }
